b79720472fa8f4ec6eae61557ab42714636033a9
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-07 16:45:01 +0600

    problem 2 solved

--- script.js
+++ script.js
@@ -1,13 +1,42 @@
-// Problem no:01
-/*function signature/sample */
-function newPrice(currentPrice , discount ) {
-          // You have to write your code here
-          let discoutnResutl = (currentPrice*discount)/100;
-        //   return discoutnResutl
-         let newTaka = currentPrice - discoutnResutl
-         return newTaka
+// Problem solve no:01
+function newPrice(currentPrice, discount) {
+    if (
+        typeof currentPrice !== "number" ||
+        typeof discount !== "number" ||
+        discount < 0 ||
+        discount > 100
+    ) {
+        return "Invalid";
+    }
+
+    let discountAmount = (currentPrice * discount) / 100;
+    let finalPrice = currentPrice - discountAmount;
+
+
+    return finalPrice.toFixed(3);
 }
-// console.log();
-let value = newPrice(2500,20);
-console.log(value);
+// console.log(newPrice(1500, 20));     
+// console.log(newPrice(2000, 15));     
+// console.log(newPrice(1200, 7));      
+// console.log(newPrice("1000", 10));   
+// console.log(newPrice(2000, 17.17));  
+// console.log(newPrice(500, "5"));  
+
+// Problem solve no:02
+function validOtp(otp) {
+    if (typeof otp !== "string") {
+        return "Invalid";
+    }
+
+    if (otp.length === 8 && otp.startsWith("ph-")) {
+        return true;
+    }
+
+    return false;
+}
+// console.log(validOtp("ph-10985"))
+// console.log(validOtp("ph-1234")); 
+// console.log(validOtp("abc-12345"))
+// console.log(validOtp(["ph-10985"]));
+// console.log(validOtp(12345678));    
 

cbc13eeb89d95aabfae2ee6815937eb46ccaa524
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-07 16:47:00 +0600

    problem 3 done

--- script.js
+++ script.js
@@ -38,5 +38,27 @@ function validOtp(otp) {
 // console.log(validOtp("ph-1234")); 
 // console.log(validOtp("abc-12345"))
 // console.log(validOtp(["ph-10985"]));
-// console.log(validOtp(12345678));    
+// console.log(validOtp(12345678));  
+
+// Problem solved no:03  
+function finalScore(omr) {
+    if (typeof omr !== "object" || Array.isArray(omr) || omr === null) {
+        return "Invalid";
+    }
+    let { right, wrong, skip } = omr;
+    if (
+        typeof right !== "number" ||
+        typeof wrong !== "number" ||
+        typeof skip !== "number"
+    ) {
+        return "Invalid";
+    }
+
+    if (right + wrong + skip !== 100) {
+        return "Invalid";
+    }
+    let score = (right * 1) - (wrong * 0.5);
+    return Math.round(score);
+}
+
 

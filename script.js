d4c23cc2189b7ec27ea9041d3c7ff9d1ae9d1bd2
Asikur Rahaman <mdasikur5893@gmail.com>
2026-02-07 16:08:05 +0600

    first problem solved code write now

---
+++ script.js
@@ -0,0 +1,11 @@
+// Problem no:01
+/*function signature/sample */
+function newPrice(currentPrice , discount ) {
+          // You have to write your code here
+          let discoutnResutl = (currentPrice*discount)/100;
+          return discoutnResutl
+}
+// console.log();
+let value = newPrice(2500,20);
+console.log(value);
+
